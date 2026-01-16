044bae94ea9b30264f1ec2485c1e3808845165f6
LamThanhPhuc <phucltse184678@fpt.edu.vn>
2026-01-16 17:04:46 +0700

    Initial commit: Microservices structure for SAMT project

---
+++ src/main/java/com/example/SAMT/SamtApplication.java
@@ -0,0 +1,13 @@
+package com.example.SAMT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SAMT/SamtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SAMT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
