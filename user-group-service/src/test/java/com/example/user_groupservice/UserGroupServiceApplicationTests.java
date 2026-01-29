cf9bd67172fbe7ff13e00d8bd98be752da4c4d2f
NeinMon <your.email@example.com>
2026-01-29 12:18:13 +0700

    config 2 service

---
+++ user-group-service/src/test/java/com/example/user_groupservice/UserGroupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_groupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserGroupServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
